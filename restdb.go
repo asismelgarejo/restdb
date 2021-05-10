ec01c24e4a4b7202de1291c9b15e529695823e16
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-10 20:12:16 +0300

    ListLogged() users

--- restdb.go
+++ restdb.go
@@ -73,8 +73,8 @@ func DeleteUser(ID int) bool {
 	return true
 }
 
-// ReturnAllUsers is for returning all users from the database table
-func ReturnAllUsers() []User {
+// ListAllUsers is for returning all users from the database table
+func ListAllUsers() []User {
 	db := ConnectPostgres()
 	if db == nil {
 		fmt.Println("Cannot connect to PostgreSQL!")
@@ -84,6 +84,38 @@ func ReturnAllUsers() []User {
 	defer db.Close()
 
 	rows, err := db.Query("SELECT * FROM users \n")
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	all := []User{}
+	var c1 int
+	var c2, c3 string
+	var c4 int64
+	var c5, c6 int
+
+	for rows.Next() {
+		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		temp := User{c1, c2, c3, c4, c5, c6}
+		all = append(all, temp)
+	}
+
+	log.Println("All:", all)
+	return all
+}
+
+// ListLogged is for returning all users from the database table
+func ListLogged() []User {
+	db := ConnectPostgres()
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		db.Close()
+		return false
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users WHERE active = 1\n")
 	if err != nil {
 		log.Println(err)
 		return nil
