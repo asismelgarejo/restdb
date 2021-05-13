5ffea9260002046f138e0584fe145edf8b2da2ca
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-13 12:06:51 +0300

    UpdateUser() Added

--- restdb.go
+++ restdb.go
@@ -157,7 +157,7 @@ func ListLogged() []User {
 	var c5, c6 int
 
 	for rows.Next() {
-		err = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
+		_ = rows.Scan(&c1, &c2, &c3, &c4, &c5, &c6)
 		temp := User{c1, c2, c3, c4, c5, c6}
 		all = append(all, temp)
 	}
@@ -349,4 +349,37 @@ func IsUserValid(u User) bool {
 		return true
 	}
 	return false
+}
+
+// UpdateUser allows you to update user name
+func UpdateUser(u User) bool {
+	log.Println("Updating user:", u)
+
+	db := ConnectPostgres()
+	if db == nil {
+		fmt.Println("Cannot connect to PostgreSQL!")
+		db.Close()
+		return false
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("UPDATE users SET Username=$1, Password=$2, Admin=$3, Active=$4 WHERE ID = $5")
+	if err != nil {
+		log.Println("Adduser:", err)
+		return false
+	}
+
+	res, err := stmt.Exec(u.Username, u.Password, u.Admin, u.Active, u.ID)
+	if err != nil {
+		log.Println("UpdateUser failed:", err)
+		return false
+	}
+
+	affect, err := res.RowsAffected()
+	if err != nil {
+		log.Println("RowsAffected() failed:", err)
+		return false
+	}
+	log.Println("Affected:", affect)
+	return true
 }
